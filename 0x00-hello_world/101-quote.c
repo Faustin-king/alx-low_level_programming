3699c38b9bc4b46de4f1d32124e0933f7252e3de
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-12 04:39:57 -0700

    Fix 101-quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <unistd.h>
 
 
 /**
@@ -10,9 +10,7 @@
 
 int main(void)
 {
-        write(int STDOUT_FILENO, "and that piece of art is useful
-
-						\" - Dora Korpar, 2015-10-19\n", 59);
+        write(5, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 
