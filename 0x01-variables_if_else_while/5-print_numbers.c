73884f392a3ea68353a1bb23917022e01260475c
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-12 10:48:51 -0700

    Fix print-numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -13,12 +13,12 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = 0; num < 10; num++);
 
-		printf("%d", num)
+		printf("%d", num);
 
 
-	printf('\n')
+	printf('\n');
 
 	return (0);
 }
