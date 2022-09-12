919b25f9b75bda2acddda8b264daf569b1c6c3c0
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-12 12:54:00 -0700

    Fix file

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -15,14 +15,18 @@ int main(void)
 	int num;
 
 	for (num = '0'; num <= '9'; num++)
+	{
 
 		putchar(num);
 
 	if (num != 9)
+	{
 		putchar(',');
 		putchar(' ');
-
+	}
 	putchar('\n');
 
 	return (0);
+
+	}
 }
