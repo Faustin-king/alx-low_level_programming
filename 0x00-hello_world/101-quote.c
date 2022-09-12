f59781cee2fbef5986f5483166652c116c348b0b
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-12 05:04:53 -0700

    Fix file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 #include <unistd.h>
 
 
@@ -8,10 +9,11 @@
  * Return: Always 1 (Success)
  */
 
-int main(void)
-{
-        write(5, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
+int main(void)
+
+{
+	write(5, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
