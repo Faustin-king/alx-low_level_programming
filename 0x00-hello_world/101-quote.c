9366f475188a1bb1429dc7fb59a32a25bfbee369
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-12 05:08:04 -0700

    Fix file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,7 +13,7 @@
 int main(void)
 
 {
-	write(5, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(4, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 
 }
