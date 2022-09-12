ae796533473b80a66cc01c2548be07d14767230b
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-12 02:34:00 -0700

    Add printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
+
+		return (0);
+
+
+}
