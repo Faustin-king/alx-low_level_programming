a9187962e81af6a8bf054285a5b4167737105f90
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-13 04:29:38 -0700

    Update _isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,31 @@
+#include <main.h>
+
+/**
+ * _isalpha -  checks for alphabetic character.
+ *
+ * @c: An input character
+ *
+ * Return: 1 if c is a letter, lowercase or uppercase
+ *  otherwise 0
+ *
+ */
+
+int _isalpha(int c)
+{
+	char lower, upper;
+
+
+	for (lower = 'a'; lower <= 'z'; lower++)
+	{
+		for (upper = 'A'; upper <= 'Z'; upper++)
+		{
+			if (c == lower || c == upper)
+
+				return (1);
+			else
+				return (0);
+		}
+	}
+
+
+}
