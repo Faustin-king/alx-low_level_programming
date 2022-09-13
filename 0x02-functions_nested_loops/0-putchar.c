d030583b32a2a811a8bed8c922456efa221ab5cd
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-13 02:30:48 -0700

    Add puchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,27 @@
+#include <unstd.h>
+#include <main.h>
+
+
+/**
+ * main - prints _putchar
+ *
+ * followed by new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	char word[8] = "_putchar";
+
+	int i;
+
+	for (i = 0; i < 9; i++)
+
+		_putchar(word[i]);
+
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,10 @@
+
+                 Functionsn and nested loops
+
+0. 0-putchar.c : Write a program that prints _putchar, followed by a new line.
+
+
+
+The program should return 0
+
+
