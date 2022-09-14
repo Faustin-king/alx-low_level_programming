a37f153ea0a6d054e9517a4c1ba560f714db7ad8
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-14 14:48:56 -0700

    Fixing

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -15,33 +15,33 @@ int print_sign(int n)
 {
 	int i;
 
+
 	for (i = 1; i >= -1; i--)
 	{
 		if (n > 0)
 		{
-			
 			_putchar('+');
-		
-				return (1);
+
+			return (1);
+
 		}
 
 		else if (n == 0)
 		{
-
 			_putchar('0');
 
-				return (0);
+			return (0);
 		}
+
 		else
 		{
 			_putchar('-');
 
-
-				return (-1);
+			return (-1);
 		}
+
+
 	}
 
-
 	return (0);
-
 }
