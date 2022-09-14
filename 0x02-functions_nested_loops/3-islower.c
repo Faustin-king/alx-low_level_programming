be6c4f6ebd3e44ca940733ff1ab35f10d5d9d35d
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-14 15:23:51 -0700

    Fixing

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -11,19 +11,5 @@
 
 int _islower(int c)
 {
-	char i;
-
-
-	for (i = 'a'; i < 'z'; i++)
-	{
-		if (c == i)
-		{
-
-			return (0);
-
-		}	return (1);
-	}
-
-	return (1);
-
+	return (c >= 'a' && c <= 'z');
 }
