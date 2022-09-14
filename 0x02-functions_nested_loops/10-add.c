b26c931c4807f08f6b6e2305f432d825581f0ef5
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-14 09:36:02 -0700

    Update add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * add - check main
+ * @i: An integer input
+ * @j: An integer input
+ * Description: adds two integers and returns the result
+ * Return: Result
+ */
+
+
+int add(int i, int j)
+{
+	int result;
+
+	result = i + j;
+
+	return (result);
+
+
+}
