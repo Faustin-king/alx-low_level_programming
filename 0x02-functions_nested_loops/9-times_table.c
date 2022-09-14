7f3f91179d80a1d5218eaa90316855f0c2daa254
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-14 09:23:37 -0700

    Fixing

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -28,13 +28,13 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');
+				_putchar(n + '0');
 			} else if (n >= 10)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
+				_putchar((n / 10) + '0');
+				_putchar((n % 10) + '0');
 			}
 		}
 
