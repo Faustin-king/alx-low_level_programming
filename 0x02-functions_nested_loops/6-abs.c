572cb04efd94b685156c2f30e5048758d9087098
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-14 07:42:32 -0700

    Update _abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#includes "main.h"
+
+/**
+ * _abs - Check main
+ *
+ * @n: An input
+ * Decription: this function returns absolute value of a number
+ * Return: Absolute value of number n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+
+		return (n);
+	else
+
+		return (n * -1);
+}
