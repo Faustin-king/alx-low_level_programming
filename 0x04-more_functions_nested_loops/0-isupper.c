2d0c0480c4383d4cd43cc124be3435d18ddc94a1
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-15 14:55:48 -0700

    Add isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _isupper - checks for uppercase character
+* @c: The character to print
+*
+* Return: 1 if c is uppercase
+* 0 otherwise
+*/
+
+
+int _isupper(int c)
+{
+	return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+		more functions, more nested loops project.
+
+file 0-isuooer.c : checks for uppercase character.
