6927d1e7e43b35cd8c2fe09adc1d1bd3aeafb422
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-15 18:43:41 -0700

    Add squares

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* print_square - prints square
+* Followed by new line
+*
+* @size: is the size of square
+* Return: Nothing
+*/
+
+
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i, j;
+
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+		}
+		_putchar('\n');
+	}
+}
