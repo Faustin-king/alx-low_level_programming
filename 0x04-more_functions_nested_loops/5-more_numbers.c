38d6c1f9d6d30a52a34e6bdd65687af700a7657f
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-15 19:38:53 -0700

    Fixing

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,10 +14,13 @@ void more_numbers(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (j = 0; j < 15; j++)
+		{
+			if (J >= 10)
 
 			_putchar(j / 10 + '0');
 
 		_putchar(j % 10 + '0');
+		}
 	}
 	_putchar('\n');
 }
