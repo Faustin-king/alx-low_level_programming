13f7f434126bfe1068440a1ac05b21249aa04e8c
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-19 12:54:27 -0700

    Add strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strlen - returns the length of a string
+* @s: input charater
+*
+* Return: The length of a string
+*/
+
+int _strlen(char *s)
+{
+	int length;
+
+	length = sizeof(*s);
+
+	return length;
+}
--- 0x05-pointers_arrays_strings/README.md
+++ 0x05-pointers_arrays_strings/README.md
@@ -13,4 +13,10 @@ FILE 1-swap.c : function that swaps the values of two integers.
 
 Prototype: void swap_int(int *a, int *b);
 
+File 2-strlen.c :  returns the length of a string.
+
+
+
+Prototype: int _strlen(char *s);
+
 
