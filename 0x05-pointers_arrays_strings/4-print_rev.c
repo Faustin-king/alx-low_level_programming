f61f834d36282ccd6ff8a8ad62aa7a7857e5034b
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-20 00:58:54 -0700

    Fix print_rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+* print_rev - prints a string
+* @s: An input character
+* Followed by new line
+*
+* Return: Nothing
+*/
+
+void print_rev(char *s)
+{
+	int len;
+
+	len = strlen(s);
+}
