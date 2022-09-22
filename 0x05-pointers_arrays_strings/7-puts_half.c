15d2f103722fd57b035ed841c2066a8450209f95
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-22 16:26:53 -0700

    Fixing

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,41 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+* puts_half - prints hailf of a string
+* Followed by new line
+* @str: string to print
+*
+* Return: Nothing
+*/
+
+
+void puts_half(char *str)
+{
+	int len, j, i;
+
+	len = 0;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (i = len / 2; str[i] != '\0'; i++)
+		{
+			_putchar(str[i]);
+		}
+
+	} else if (len % 2)
+	{
+		for (j = (len - 1) / 2; j < len - 1; j++)
+		{
+			_putchar(str[j + 1]);
+		}
+	}
+	_putchar('\n');
+}
+
