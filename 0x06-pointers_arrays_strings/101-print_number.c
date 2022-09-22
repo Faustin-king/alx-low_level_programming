54fd1cc4c3f75cb337465f6b21b476b8b4e03725
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-22 16:14:44 -0700

    Update print_number

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+
+
+/**
+* print_number - prints an integer;
+* @n: integer to be printed;
+*/
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+
+		putchar('-');
+	} else
+	{
+
+		n1 = n;
+	}
+
+	if (n1 / 10)
+
+		print_number(n1 / 10);
+
+	putchar((n1 % 10) + '0');
+}
+
