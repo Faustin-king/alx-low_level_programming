b62d72cf87f5bffa04df27e339dc20eaa1a88250
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-22 16:22:06 -0700

    Update main

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,7 +9,8 @@ void reverse_array(int *a, int n);
 char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
-
+void print_number(int n);
+char *rot13(char *);
 
 
 #endif /* MAIN_H */
