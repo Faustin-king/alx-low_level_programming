bc69fc9c112af48c8542205d8238f0bf83e4a3ba
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-22 16:28:47 -0700

    Fixing

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -26,16 +26,16 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			_putchar(str[i]);
+			putchar(str[i]);
 		}
 
 	} else if (len % 2)
 	{
 		for (j = (len - 1) / 2; j < len - 1; j++)
 		{
-			_putchar(str[j + 1]);
+			putchar(str[j + 1]);
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
