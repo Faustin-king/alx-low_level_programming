8da0d6f144e82b9d3e5c6b36ce8354ce9319ac4e
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-09-28 11:52:47 -0700

    Update file

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-*main - test function that prints if integer is positive or negative
+* main - test function that prints if integer is positive or negative
 *
 *Return: 0
 */
@@ -13,6 +13,7 @@ int main(void)
 
 
 	i = 0;
+
 	positive_or_negative(i);
 
 	return (0);
