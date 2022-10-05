46cc613687717ed8e55c96c917064bdc5891d5c9
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-10-05 09:01:01 -0700

    Add args

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -4,5 +4,5 @@
 int _putchar(char c);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
-
+int main(int argc, char *argv[]);
 #endif /* MAIN_H */
