1e658e70075a837b04b6fa1fe0d55a837535a2a4
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-10-05 06:56:14 -0700

    Add args

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+
+/**
+* main - prints the number of arguments passed into it.
+* @argc: number of arguments
+* @argv: array of arguments
+*
+* Return: Always 0 (Success)
+*/
+
+int main(int argc, char* argv[] __attribute__((unused)))
+{
+	printf("number of arguments = %d\n", argc);
+	return (0);
+}
