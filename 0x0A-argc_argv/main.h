8415e6adf0387cec1fcd5f4791e0b3dcd8163aab
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-10-05 09:32:38 -0700

    Add mul

--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -5,4 +5,5 @@ int _putchar(char c);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
 int main(int argc, char *argv[]);
+int _atoi(char *s);
 #endif /* MAIN_H */
