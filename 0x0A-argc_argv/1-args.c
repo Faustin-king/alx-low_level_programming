3a6fc7bfd79c96fbd22cc742cb6099e5a0dc5ee9
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-10-05 08:04:55 -0700

    Fixing

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -11,7 +11,7 @@
 * Return: Always 0 (Success)
 */
 
-int main(int argc, char* argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("number of arguments = %d\n", argc - 1);
 	return (0);
