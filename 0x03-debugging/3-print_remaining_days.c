f4bc6f565562817ecbc06d286f27dca4a050fa5d
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-10-15 04:31:47 -0700

    Fixing

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -16,7 +16,7 @@ void print_remaining_days(int month, int day, int year)
 	if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
 	{
 
-		if (month >= 2 && day >= 60)
+		if (month > 2)
 		{
 			day++;
 		}
@@ -30,7 +30,7 @@ void print_remaining_days(int month, int day, int year)
 	else
 	{
 
-		if ((month <= 1 || month >= 12) && (day <= 1 || day >= 31))
+		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
 		}
