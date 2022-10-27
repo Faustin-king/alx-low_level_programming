eede85fa51ecc6f891ef5942b160325ffff263bc
Faustin-king <f.nduwumwamiwabami@gmail.com>
2022-10-27 08:03:04 -0700

    Fixing

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "dog.h"
 
 
@@ -19,9 +18,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 		d = malloc(sizeof(struct dog));
 
 	d->name = name;
-
 	d->age = age;
-
 	d->owner = owner;
 
 }
