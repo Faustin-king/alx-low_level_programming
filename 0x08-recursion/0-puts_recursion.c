05eb3a52b0ab007b4e595555d29c0fb4106687d4
Faustin-king <f.nduwumwamiwabami@gmail.com>
2023-08-09 06:52:15 -0700

    Add recursion files

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,23 @@
+#include"main.h"
+#include"stdio.h"
+
+
+/**
+ * _puts_recursion - a function that prints a string, followed by a new line.
+ *
+ * s : a string to be printed out.
+ *
+ * Return: Nothing.
+ */
+
+
+void _puts_recursion(char *s) {
+	
+	if(*s == '\0') {
+		putchar('\n');
+			
+			return;
+	}
+	putchar(*s);
+
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,3 @@
+# 0x08. C - Recursion
+
+### File: 0-puts_recursion.c: Write a function that prints a string, followed by a new line.
