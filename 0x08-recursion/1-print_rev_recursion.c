28e293b6779fcdafbd0b84f444fb2631c5688b5c
Faustin-king <f.nduwumwamiwabami@gmail.com>
2023-08-10 01:21:56 -0700

    Update function

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -13,7 +13,7 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
-        return; /* Base case: end of the string*/
+	return; /* Base case: end of the string*/
 
 	_print_rev_recursion(s + 1); /* Recur for the rest of the string*/
 	printf("%c", *s); /* Print the current character after the recursive call*/
